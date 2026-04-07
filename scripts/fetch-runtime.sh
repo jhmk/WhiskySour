929546a366615d4f7e274a86c63e8f81a886ea5e
JHMK <jankoch+github@mailbox.org>
2026-04-07 17:09:58 +0200

    Update fetch runtime to latest Wine

--- scripts/fetch-runtime.sh
+++ scripts/fetch-runtime.sh
@@ -5,7 +5,7 @@ REPO="$(cd "$(dirname "$0")/.." && pwd)"
 LIBDIR="$REPO/Libraries"
 TARBALL="$REPO/Libraries.tar.gz"
 
-WINE_URL="https://github.com/Gcenx/macOS_Wine_builds/releases/download/11.0-rc1/Gcenx-wine-11.0-rc1.tar.xz"
+WINE_URL="https://github.com/Gcenx/macOS_Wine_builds/releases/download/11.0/wine-stable-11.0-osx64.tar.xz"
 DXVK_URL="https://github.com/doitsujin/dxvk/releases/download/v2.7.1/dxvk-2.7.1.tar.gz"
 CABEXTRACT_URL="https://github.com/baskerville/cabextract/releases/download/1.12/cabextract-1.12.tar.gz"
 
